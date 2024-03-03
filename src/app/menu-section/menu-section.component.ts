12829eff4641bcae1fd0b254c2489860d6418ee4
ouayoub <ouayoub.code@gmail.com>
2024-03-03 11:14:42 +0100

    add menu section

--- src/app/menu-section/menu-section.component.ts
+++ src/app/menu-section/menu-section.component.ts
@@ -5,6 +5,51 @@ import { Component } from '@angular/core';
   templateUrl: './menu-section.component.html',
   styleUrl: './menu-section.component.css'
 })
-export class MenuSectionComponent {
+export class MenuSectionComponent  {
+
+  name1 = "Salade ";
+  name2 = " Classic Burgers";
+  name3 = "Classic pizzas ";
+  name4 = "Appetizers "
+
+
+
+  Salade = [ 
+  
+  'Salade César poulet' ,
+  'Assiette de saumon fumé' ,
+  'Salade niçoise revisitée' ,
+  'Planche de fromage'
+
+   ]
+
+  Burgers = [
+
+  "Cheeseburger",
+  "Bacon Cheeseburger",
+  "Double Cheeseburger",
+  "Mushroom & Swiss Burger"
+    ]
+
+  pizzas = [
+
+      "Margherita",
+      "Hawaiian",
+      "Mushroom",
+      "Truffle pizza"
+  ]
+
+  Appetizers = [
+
+      "Oysters on the half shell",
+      "Shrimp cocktail",
+      "Mussels",
+      "Calamari fritti"
+   ]
+
+  images = {
+    leftImg : "../../assets/1.png",
+    rightImg : "../../assets/2.png"
+  }
 
 }
