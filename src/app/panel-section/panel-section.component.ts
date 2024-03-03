da5d02a482e32ea4705d841208fd817953e14537
ouayoub <ouayoub.code@gmail.com>
2024-03-03 11:15:42 +0100

    panel section

--- src/app/panel-section/panel-section.component.ts
+++ src/app/panel-section/panel-section.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class PanelSectionComponent {
 
+  Leftcontent = {
+    leftText : "it's very important to make healthy kitchen for fresh food",
+    title : "Book a Table",
+    img : "../../assets/f.png"
+  }
+
+ Rightcontent = {
+    RightText : "great food can mak your all party more enjoyable",
+    title : "Book a Table",
+    img : "../../assets/h.png"
+
+  }
+
 }
